136eac105b7885802c3c79cbe1e0a416383baad3
Breese <breese@specialized-computers.com>
2014-12-02 15:45:20 -0500

    fix order to db fields

--- src/migrations/2014_12_02_151606_add_username_to_statistics_table.php
+++ src/migrations/2014_12_02_151606_add_username_to_statistics_table.php
@@ -14,8 +14,8 @@ class AddUsernameToStatisticsTable extends Migration {
 	{
 		Schema::table('statistics', function(Blueprint $table)
 		{
-			$table->string('firstname', 50)->nullable()->after('userid');
-			$table->string('lastname', 50)->nullable()->after('firstname');
+			$table->string('firstname', 50)->after('userid')->nullable();
+			$table->string('lastname', 50)->after('firstname')->nullable();
 		});
 	}
 
