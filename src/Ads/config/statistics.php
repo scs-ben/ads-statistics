eafc70f470c04d791fb7ab0bce5f72b7398003ed
Ben <breese@scsgrandrapids.com>
2015-03-20 15:00:30 -0400

    updates for l5 stats tracker

---
+++ src/Ads/config/statistics.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Settings
+	| for user_id, first_name, last_name - set the db column name that corresponds the field
+	|  -> the user_id field is a VARCHAR(50) so an email could be stored
+	| protected_fields: array that holds sensitive data fields to strip out of the input dump to the db
+	|--------------------------------------------------------------------------
+	*/
+
+	'user_id' => null,
+    'first_name' => null,
+    'last_name' => null,
+	'protected_fields' => ['password'],
+    
+);
