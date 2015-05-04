2ea1bf3dea9b648aa0b018d80ccfe34353dc0cd7
BR <breese@specialized-computers.com>
2015-05-04 12:18:36 -0400

    Create StatisticHandler.php
    test way to log errors without modifying laravel exception handler file

---
+++ src/Ads/Statistics/StatisticHandler.php
@@ -0,0 +1,29 @@
+<?php namespace Ads\Statistics;
+
+use Statistic;
+
+class StatisticHandler extends \Illuminate\Foundation\Exceptions\Handler {
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception  $e
+	 * @return void
+	 */
+	public function report(Exception $e)
+	{
+		return parent::report($e);
+	}
+	
+	public function render($request, Exception $e)
+	{
+		if ($this->isHttpException($e)) {
+			Statistic::httpError($request, $e);
+		} else {
+			Statistic::fatalError($request, $e);
+		}
+	
+		return parent::render($request, $e);
+	}
+}
