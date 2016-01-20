2999fb4ae8f37b60f84beb57249784867ad32d52
BR <breese@specialized-computers.com>
2016-01-20 12:55:06 -0500

    add error stack

--- src/Ads/Statistics/Statistic.php
+++ src/Ads/Statistics/Statistic.php
@@ -20,7 +20,7 @@ class Statistic extends \Eloquent {
 		$statistic->http_code = http_response_code();
 		$statistic->errorFile = $e->getFile();
 		$statistic->errorLine = $e->getLine();
-		$statistic->errorMessage = $e->getMessage() . '\r\nTRACE\r\n' . $e->getTraceAsString();
+		$statistic->errorMessage = $e->getMessage() . PHP_EOL . 'TRACE' . PHP_EOL . $e->__toString();
 		
 		$statistic->save();
 	}
@@ -32,7 +32,7 @@ class Statistic extends \Eloquent {
 		$statistic->http_code = http_response_code();
 		$statistic->errorFile = $e->getFile();
 		$statistic->errorLine = $e->getLine();
-		$statistic->errorMessage = $e->getMessage() . '\r\nTRACE\r\n' . $e->getTraceAsString();
+		$statistic->errorMessage = $e->getMessage() . PHP_EOL . 'TRACE' . PHP_EOL . $e->__toString();
 		
 		$statistic->save();
 	}
